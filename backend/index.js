2b861d2a53097dd02da50793a72c0641fdba3089
Utsav Agrawal <agrutsav88728@gmail.com>
2023-08-30 13:41:20 +0530

    nextone

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,31 @@
-const express=require("express")
-const app=express();
-const dotenv= require('dotenv')
-dotenv.config()
-const mongoDB=require("./db")
-mongoDB()
-const cors=require("cors")
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoDB = require("./db");
+mongoDB();
+const cors = require("cors");
 app.use(cors());
 
-app.get("/",(req,res)=>{
-    res.send("hello world")
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.UseCors((x) =>
+  x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed((origin) => true) // allow any origin
+    .AllowCredentials()
+);
 
-})
-app.use(express.json())
-app.use('/api',require('./Routes/CreateUser'));
-app.use('/api',require('./Routes/DisplayData'));
-app.use('/api',require('./Routes/OrderData'));
-app.get("/",(req,res)=>{
-    res.send("hello")
-})
-const port= process.env.PORT || 8800;
-app.listen(port,()=>{
-    console.log("Connected successfully")
-    
-})
+app.use(express.json());
+app.use("/api", require("./Routes/CreateUser"));
+app.use("/api", require("./Routes/DisplayData"));
+app.use("/api", require("./Routes/OrderData"));
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+const port = process.env.PORT || 8800;
+app.listen(port, () => {
+  console.log("Connected successfully");
+});
